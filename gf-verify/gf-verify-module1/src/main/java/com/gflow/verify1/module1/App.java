10e32070a439e204c05a7be6faf9016eb7895556
Desmond Kirrane <desmond.kirrane@gmail.com>
2014-04-01 21:48:30 +0100

    Initial commit

---
+++ gf-verify/gf-verify-module1/src/main/java/com/gflow/verify1/module1/App.java
@@ -0,0 +1,8 @@
+package com.gflow.verify1.module1;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Module1!");
+    }
+}
---
+++ gf-verify/gf-verify-module2/src/main/java/com/gflow/verify1/module2/App.java
@@ -0,0 +1,8 @@
+package com.gflow.verify1.module2;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Module2!");
+    }
+}
